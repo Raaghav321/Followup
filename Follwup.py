0ca3a0f923e1cac3f9d711847e7408614babadb4
Raaghav <rawasthi2005@gmail.com>
2019-10-02 08:25:23 -0500

    Update Follwup.py

---
+++ Follwup.py
@@ -0,0 +1,20 @@
+#print("hello world")
+
+print("Howdy y'all.")
+print("I like typing this.")
+print("This is fun.")
+
+# math skills demo
+print("I will not count my chickens", 2)
+print("Hens", 25+30/6)
+print("Roosters: " , 100 - 25*3 % 4)
+print("Now I will count the eggs" , 7)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+#math
+print("I will count my pens")
+print("Pens", 28+8/2 )
+print("I will count my binders ")
+print("binders", 58+92-45)
+print("I will cont my eggs", 8)
+print("Eggs", 3+8-5+3+6-8-9+10/2)
