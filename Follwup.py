596fa2271adf3dee3a17a5b34ea140cb1d70685a
Raaghav <rawasthi2005@gmail.com>
2019-10-04 08:34:52 -0500

    Update Follwup.py

--- Follwup.py
+++ Follwup.py
@@ -18,3 +18,38 @@ print("I will count my binders ")
 print("binders", 58+92-45)
 print("I will cont my eggs", 8)
 print("Eggs", 3+8-5+3+6-8-9+10/2)
+
+#Variables and some of their powers
+cars=100
+spaceInACar= 4.0
+drivers=30
+passengers=90
+cars_not_driven=cars-drivers
+cars_driven=drivers
+carpool_capacity=spaceInACar * cars_driven
+average_passengers_per_car = passengers / cars_driven
+
+print("There are",cars,"cars available")
+print("There are only",drivers, "available today")
+print("There will be", cars_not_driven,"drivers available today")
+print("We can transport", carpool_capacity,"people today")
+print("we have",passengers,"to carpool today")
+print("we have to put aproximateley",average_passengers_per_car,"in each car")
+
+#Variables and some of their powers
+watermelons=50
+watermelons_not_eaten=30
+watermelons_eaten=20
+
+# More variables and playing with output
+myName = "Raaghav"
+myAge = 14
+myHeight = 62.5 # inches
+myEyes = "brown"
+myHair = "black-brown"
+
+print("Let's talk about %s." %myName)
+print("He's %d inches tall." % myHeight)
+print("He's got %s eyes and hair" %(myEyes, myHair))
+print("add %d and %d, I get d%" %(myAge, myHeight, myAge+myMeight))
+
